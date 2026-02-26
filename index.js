1f10531a2ed3b11f12b45ea6f65ad3f7e777cdd4
Atharv Agarwal <atharv20agarwal@gmail.com>
2026-02-26 22:55:58 +0530

    feat: Completed script and moved creds to env for safety

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { Builder, Browser, By, Key } = require("selenium-webdriver");
+require("dotenv").config();
+const { Builder, Browser, By, Key, until } = require("selenium-webdriver");
 
 async function AutoLoginHandler() {
   //   let driver = await new Builder().forBrowser(Browser.CHROME).build();
@@ -6,13 +7,28 @@ async function AutoLoginHandler() {
 
   try {
     await driver.get("http://captive.apple.com");
-    await driver.findElement(By.id("username")).sendKeys("username");
-    await driver.findElement(By.name("password")).sendKeys("password");
-    // await driver.findElement(By.name("login")).click();
+
+    let usernameField = await driver.wait(
+      until.elementLocated(By.name("username")),
+      10000,
+    );
+    await usernameField.sendKeys(process.env.USERNAME);
+
+    let passwordField = await driver.wait(
+      until.elementLocated(By.name("password")),
+      10000,
+    );
+    await passwordField.sendKeys(process.env.PASSWORD);
+
+    let continueBtn = await driver.wait(
+      until.elementLocated(By.xpath("//button[@type='submit']")),
+      10000,
+    );
+    await continueBtn.click();
   } catch (err) {
     console.log(err);
   } finally {
-    await driver.sleep(5000);
+    await driver.sleep(500);
     await driver.quit();
   }
 }
